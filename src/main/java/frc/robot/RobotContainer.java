a120054e8dc76859b6b6250f5aacecc858b955f3
RHSMcLain <amclain@riverdale.k12.or.us>
2024-10-10 14:23:55 -0700

    fixed swapped joystick/button values

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -29,8 +29,8 @@ public class RobotContainer {
   // Replace with CommandPS4Controller or CommandJoystick if needed
   // private final CommandXboxController m_driverController =
   //     new CommandXboxController(OperatorConstants.kDriverControllerPort);
-  private final Joystick controller = new Joystick(1);
-  private final JoystickButton trigger = new JoystickButton(controller, 0);
+  private final Joystick controller = new Joystick(0);
+  private final JoystickButton trigger = new JoystickButton(controller, 1);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
